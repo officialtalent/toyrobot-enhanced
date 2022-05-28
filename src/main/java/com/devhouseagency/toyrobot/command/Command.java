9bf2aa6261a1d1b14f0ea642a2ee47c301a631e5
officialtalent <talentsqwabe@gmail.com>
2022-05-28 10:14:52 +0200

    Refactored Command to support JDK 11 syntax

--- src/main/java/com/devhouseagency/toyrobot/command/Command.java
+++ src/main/java/com/devhouseagency/toyrobot/command/Command.java
@@ -29,16 +29,26 @@ public abstract class Command {
     public static Command create(String instruction) {
         String[] args = instruction.trim().toLowerCase().split(" ", 2);
 
-        return switch (args[0]) {
-            case "shutdown", "off" -> new ShutdownCommand();
-            case "forward" -> new ForwardCommand(args[1]);
-            case "back" -> new BackCommand(args[1]);
-            case "help" -> new HelpCommand();
-            case "right" -> new RightCommand();
-            case "left" -> new LeftCommand();
-            case "sprint" -> new SprintCommand(args[1]);
-            case "replay" -> new ReplayCommand(args.length > 1 ? args[2] : "");
-            default -> throw new IllegalArgumentException("Unsupported command " + instruction);
-        };
+        switch (args[0]) {
+            case "shutdown":
+            case "off":
+                return new ShutdownCommand();
+            case "forward":
+                return new ForwardCommand(args[1]);
+            case "back":
+                return new BackCommand(args[1]);
+            case "help":
+                return new HelpCommand();
+            case "right":
+                return new RightCommand();
+            case "left":
+                return new LeftCommand();
+            case "sprint":
+                return new SprintCommand(args[1]);
+            case "replay":
+                return new ReplayCommand(args.length > 1 ? args[2] : "");
+            default:
+                throw new IllegalArgumentException("Unsupported command " + instruction);
+        }
     }
 }
